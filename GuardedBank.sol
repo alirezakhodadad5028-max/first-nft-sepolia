7e5ce5bc99f40a053c31a17354fea9b403ae9cc6
Alireza <alireza.khodadad5028@gmail.com>
2026-09-19 13:15:34 +0330

    Add files via upload

---
+++ GuardedBank.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+contract GuardedBank is ReentrancyGuard {
+    mapping(address => uint256) public balances;
+
+    function deposit() public payable {
+        balances [msg.sender] += msg.value;
+    }
+    function withdraw () public nonReentrant {
+        uint256 amount = balances [msg.sender];
+       balances [msg.sender] = 0;
+
+       (bool success, ) = msg.sender.call{value: amount} ("");
+       require(success, "Transfer failed.");
+    }
+}
